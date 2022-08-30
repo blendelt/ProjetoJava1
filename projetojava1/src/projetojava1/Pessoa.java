9baca5c3062b09803963c56f9565fc7bc4367a0b
Blende <Blende@DESKTOP-MSNB63R>
2022-08-30 09:33:21 -0300

    Adição da Classe Pessoa

---
+++ projetojava1/src/projetojava1/Pessoa.java
@@ -0,0 +1,30 @@
+package projetojava1;
+
+public class Pessoa {
+	
+	private int idade;
+	private double altura;
+	private String nome;
+	
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+
+	
+}
